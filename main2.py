f40b7b42faf3a2a22b8af2bafc0a9ee3296f0747
Baerett Stone <baerstone3@gmail.com>
2021-02-01 15:07:13 -0500

    Changed main and main2

--- main2.py
+++ main2.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 import sys
+import random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+number = random.randrange(1,11)
+guess = input("Guess a number from 1 to 10: ")
+while not guess.isnumeric:
+    guess = input("Invalid input. Guess a number from 1 to 10")
+    
+guess = int(guess)
+if guess == number:
+    print("Great job! You got it!")
+else:
+    print("Sorry, better luck next time.")
+    print("The number was " + str(number))
